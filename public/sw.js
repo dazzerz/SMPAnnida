ab814176788fb954e74d9da7255670f3df2f6274
dazzerz <94317647+dazzerz@users.noreply.github.com>
2026-09-26 18:22:21 +0700

    fix: remove smoothRedirect link hijacker and fix 1024px CSS overlap

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('install', () => { self.skipWaiting(); });
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
